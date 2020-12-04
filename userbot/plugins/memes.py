218f3ba4ce1f84e02428a163adac4a4291361400
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-04 09:08:44 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -667,7 +667,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("[Marshmellow Userbot](https://github.com/Marshmellow098/MY-THINGS)")
+        await e.edit("[Marshmellow Userbot](https://github.com/Marshmellow098/MARSHMELLOW-USERBOT.git)")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
