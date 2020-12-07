c50a63c26e5ebec73f36036f58e974db974dd22c
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-07 10:04:02 +0530

    Add files via upload

---
+++ userbot/plugins/Xogame.py
@@ -0,0 +1,18 @@
+"""
+Took from @HellBot_Offical made by @Kraken_The_BadASS
+"""
+
+from userbot.utils import admin_cmd,edit_or_reply
+from var import Var
+
+@borg.on(admin_cmd(pattern="xogame$"))
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    mellow = "play"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, mellow) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
