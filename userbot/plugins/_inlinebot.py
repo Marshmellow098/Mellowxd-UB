d03bfc879199eb7156dc919d57091d29cb058e0c
ahirearyan2 <66762638+ahirearyan2@users.noreply.github.com>
2020-12-08 22:09:14 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -102,7 +102,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    if len(modules) % number_of_cols == 1:
+    if len(modules) % number_of_cols == 3:
         pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
