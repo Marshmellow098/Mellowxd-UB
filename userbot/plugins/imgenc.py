e1b4bbb41d67bb85f138514487ccac7a8d67e94c
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-13 17:10:30 +0530

    Fixed

--- userbot/plugins/imgenc.py
+++ userbot/plugins/imgenc.py
@@ -82,11 +82,5 @@ async def _(event):
       file=result)
 
 
-CMD_HELP.update(
-    {
-        "imgenc": 
-    ".enc <reply to any anime media> "
-    "\nIt enhancerize any anime pic (Note :-if its not working then go to deepai.org then get api and set var DEEP_AI nd key.)"
-    })
 
 #hehe
