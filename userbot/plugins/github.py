6b9162712fd0c31ebabad96022d35b6a0bfe380e
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-14 13:51:41 +0530

    Update github.py

--- userbot/plugins/github.py
+++ userbot/plugins/github.py
@@ -5,7 +5,7 @@ import requests
 from userbot.utils import admin_cmd
 
 
-@borg.on(admin_cmd("github (.*)"))
+@mellow.on(admin_cmd("github (.*)"))
 async def _(event):
     if event.fwd_from:
         return
