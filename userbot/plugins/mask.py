3d597be42f2b1f7d1b30a1b5bd7baaed29a4d658
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-14 15:33:09 +0530

    Me got irritated

--- userbot/plugins/mask.py
+++ userbot/plugins/mask.py
@@ -2,7 +2,7 @@ import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbt.utils import admin_cmd
+from userbot.utils import admin_cmd
 
 @mellow.on(admin_cmd("mask ?(.*)"))
 async def _(event):
