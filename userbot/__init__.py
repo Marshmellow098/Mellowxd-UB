ba15caffdae1f719a9d719728ff4328766167fb2
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-17 13:07:12 +0530

    Done

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -10,6 +10,7 @@ os.system("pip install --upgrade pip")
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
     bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    mellow = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
