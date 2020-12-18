8911357c6eb59a32c4fbec49508149c9da7ad060
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2020-12-18 12:12:14 +0530

    @Mellowxd

---
+++ userbot/plugins/Animal.py
@@ -0,0 +1,92 @@
+"""
+MADE BY @Stark_TVL 
+bug fixed by @Mellowxd
+"""
+
+import asyncio
+import os
+from userbot.utils import mellow_cmd
+
+M = ("┈┈╱▔▔▔▔▔▔▔▔▔▔▔▏\n" 
+     "┈╱╭▏╮╭┻┻╮╭┻┻╮╭▏ \n"
+     "▕╮╰▏╯┃╭╮┃┃╭╮┃╰▏ \n"
+     "▕╯┈▏┈┗┻┻┛┗┻┻┻╮▏ \n"
+     "▕╭╮▏╮┈┈┈┈┏━━━╯▏\n" 
+     "▕╰╯▏╯╰┳┳┳┳┳┳╯╭▏ \n"
+     "▕┈╭▏╭╮┃┗┛┗┛┃┈╰▏ \n"
+     "▕┈╰▏╰╯╰━━━━╯┈┈▏\n")
+
+@mellow.on(mellow_cmd(pattern=r"spong"))
+async def kek(mellow):
+  await mellow.edit(M)
+ 
+  D = ("╭━┳━╭━╭━╮╮\n"
+       "┃┈┈┈┣▅╋▅┫┃\n"
+       "┃┈┃┈╰━╰━━━━━━╮\n"
+       "╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣\n"
+       "╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉\n"
+       "╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤\n"
+       "╲┃┈┈┈┈╭━┳━━━━╯\n"
+       "╲┣━━━━━━┫\n")
+       
+@mellow.on(mellow_cmd(pattern=r"spong"))
+async def dog(bog):
+  await dog.edit(D)
+  P = ("┈┏━╮╭━┓┈╭━━━━╮\n"
+       "┈┃┏┗┛┓┃╭┫ⓞⓘⓝⓚ┃\n"
+       "┈╰┓▋▋┏╯╯╰━━━━╯\n"
+       "╭━┻╮╲┗━━━━╮╭╮┈\n"
+       "┃▎▎┃╲╲╲╲╲╲┣━╯┈\n"
+       "╰━┳┻▅╯╲╲╲╲┃┈┈┈\n"
+       "┈┈╰━┳┓┏┳┓┏╯┈┈┈\n"
+       "┈┈┈┈┗┻┛┗┻┛┈┈┈┈\n")
+       
+   
+      
+  
+F =(" ╱▏┈┈┈┈┈┈▕╲▕╲┈┈┈\n"
+     "▏▏┈┈┈┈┈┈▕▏▔▔╲┈┈\n"
+     "▏╲┈┈┈┈┈┈╱┈▔┈▔╲┈\n"
+    "╲▏▔▔▔▔▔▔╯╯╰┳━━▀\n"
+    "┈▏╯╯╯╯╯╯╯╯╱┃┈┈┈\n"
+    "┈┃┏┳┳━━━┫┣┳┃┈┈┈\n"
+    "┈┃┃┃┃┈┈┈┃┃┃┃┈┈┈\n"
+    "┈┗┛┗┛┈┈┈┗┛┗┛┈┈┈\n")
+    
+E =("┈┈┈┈╱▔▔▔▔▔╲┈╱▔╲\n"
+    "┈┈┈┈▏┈┈▏╭╮▕┈▏╳▕\n"
+    "┈┈┈┈▏┈┈▏┈┈▕┈╲▂╱\n"
+    "┈╱▔▔╲▂╱╲▂▂┈╲▂▏▏\n"
+    "╭▏┈┈┈┈┈┈┈▏╲▂▂╱┈\n"
+    "┃▏┈┈┈┈▏┈┈▏┈┈┈┈┈\n"
+    "╯▏┈╲╱▔╲▅▅▏┈┈┈┈\n"
+    "┈╲▅▅▏▕▔▔▔▔▏┈┈┈┈\n")
+    
+H = ("┈┈┈╭━━━━━╮┈┈┈┈┈\n"
+     "┈┈┈┃┊┊┊┊┊┃┈┈┈┈┈\n"
+     "┈┈┈┃┊┊╭━╮┻╮┈┈┈┈\n"
+     "┈┈┈╱╲┊┃▋┃▋┃┈┈┈┈\n"
+      "┈┈╭┻┊┊╰━┻━╮┈┈┈┈\n"
+       "┈┈╰┳┊╭━━━┳╯┈┈┈┈\n"
+       "┈┈┈┃┊┃╰━━┫┈HOMER\n"
+       "┈┈┈┈┈┈┏━┓┈┈┈┈┈┈\n")
+      
+    
+  
+@mellow.on(mellow_cmd(pattern=r"fox"))
+async def fox(fox):
+  await fox.edit(F)
+  
+@mellow.on(mellow_cmd(pattern=r"elephant"))
+async def elephant(elephant):
+  await elephant.edit(E)
+
+  
+@mellow.on(mellow_cmd(pattern=r"homer"))
+async def homer(homer):
+  await homer.edit(H)
+
+@mellow.on(mellow_cmd(pattern=r"pig"))
+async def pig(pig):
+  await pig.edit(P)
+  
